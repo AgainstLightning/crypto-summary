b496c23ee37f5527f960f53e365e3a7e51692b82
AgainstLightning <againstlightning@gmail.com>
2021-11-25 02:37:54 -0500

    add hydrator to api summary route

--- pages/api/summary.ts
+++ pages/api/summary.ts
@@ -7,7 +7,7 @@ const WHITELIST = ["btcusd", "ethusd"]
 const handler = async (req : NextApiRequest, res : NextApiResponse) => {
     const apiResponse = await fetch(URL)
     const data : FetchedResponse = await apiResponse.json()
-    const hydrated : HydratedData = hydrator(data)
+    const hydrated = hydrator(data)
 
     res.status(200).json(hydrated)
 }
@@ -17,12 +17,13 @@ const hydrator = (data : FetchedResponse) : HydratedData => {
 
     for (let marketPair in data.result) {
         const [market, pair] = marketPair.split(":")
+        const isOnWhitelist = WHITELIST.includes(pair)
+        const isNewPair = !Object.keys(hydrated).includes(pair)
 
-        if(!WHITELIST.includes(pair)) continue
+        if(!isOnWhitelist) continue
+        if(isNewPair) hydrated[pair] = {}
 
-        if(!hydrated.hasOwnProperty(pair)) hydrated[pair] = {}
-
-        hydrated[pair][market] = {market, pair, ...data.result}
+        hydrated[pair][market] = { market, pair, ...data.result[marketPair] }
     }
 
     return hydrated
